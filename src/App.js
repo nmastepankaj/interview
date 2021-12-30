e5ebe015729d0697a25fbd7f10feda43a41bce9c
Pankaj Singh Rana <pankajsinghrana2003@gmail.com>
2021-12-30 13:41:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Heading from './components/Heading';
+import Switchable from './components/Switchable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  
+
+  render() {
+    return (
+      <div className='main_body'>
+        <Heading/>
+        <Switchable/>
+      </div>
+    )
+  }
 }
-
-export default App;
